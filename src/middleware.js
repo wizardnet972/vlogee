ed8caf49d6dcef3dc901235d37542c97d35fb2ac
wizardnet972 <wizardnet972@gmail.com>
2019-08-25 19:45:14 +0300

    init

---
+++ src/middleware.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const ejs = require('ejs');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const logsHtml = fs.readFileSync(path.resolve(__dirname, './logs.ejs'), 'utf-8');
+  const logs = fs.readdirSync('./logs').map(file => file);
+  const html = ejs.render(logsHtml, { logs });
+  res.send(html);
+});
+
+router.get('/:id', (req, res) => {
+  const d = fs.readFileSync(`./logs/${req.params.id}`, 'utf-8');
+
+  const logs = d
+    .split('\n')
+    .filter(s => !!s)
+    .map(s => JSON.parse(s))
+    .reverse();
+
+  res.json(logs);
+});
+
+module.exports = { router };
